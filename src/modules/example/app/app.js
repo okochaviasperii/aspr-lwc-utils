cb1beab9fa0f26f12fd25f102db942d4c5523cc9
Omri Kochavi <nomrik@gmail.com>
2023-01-12 10:22:25 +0000

    routing example

--- src/modules/example/app/app.js
+++ src/modules/example/app/app.js
@@ -1,3 +1,32 @@
 import { LightningElement } from 'lwc';
+import { createRouter } from 'lwr/router';
 
-export default class HelloWorldApp extends LightningElement {}
+// Route definition array
+const routes = [
+    {
+        id: 'home',
+        uri: '/',
+        handler: () => import('example/homePageHandler'),
+        page: {
+            type: 'home',
+        },
+    },
+    {
+        id: 'namedPage',
+        uri: '/:pageName',
+        handler: () => import('example/namedPageHandler'),
+        page: {
+            type: 'namedPage',
+            attributes: {
+                pageName: ':pageName',
+            },
+        },
+    },
+];
+
+export default class SimpleRoutingApp extends LightningElement {
+    router = createRouter({ routes });
+
+    homeReference = { type: 'home' }; // Home page reference for the page-link
+    testReference = { type: 'namedPage', attributes: { pageName: 'test' } }
+}
