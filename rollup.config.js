f9dab46249f49488906a0ae3c9a5edb7de25fbd9
Omri Kochavi <nomrik@gmail.com>
2023-01-16 10:56:02 +0000

    sfdx project

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
-export default {
-    input: 'src/modules/lib/asprLwcUtils/asprLwcUtils.js',
-    output: {
-        file: 'dist/asprLwcUtils.js',
-        format: 'iife',
-        name: 'asprLwcUtils'
-    }
-}
+export default [
+    {
+        input: 'src/modules/lib/asprLwcUtils/asprLwcUtils.js',
+        output: {
+            file: 'dist/asprLwcUtils.js',
+            format: 'iife',
+            name: 'asprLwcUtils'
+        }
+    },
+    {
+        input: 'src/modules/lib/asprLwcUtils/asprLwcUtils.js',
+        output: {
+            file: 'package/force-app/main/default/staticresources/asprLwcUtils.js',
+            format: 'iife',
+            name: 'asprLwcUtils'
+        }
+    },
+]
